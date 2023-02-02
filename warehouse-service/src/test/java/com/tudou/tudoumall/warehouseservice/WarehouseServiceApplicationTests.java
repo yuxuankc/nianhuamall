26e5c37866e129ba0ecc56e044dc83bed2bf91d5
kaicai <cai_kai@hotmail.com>
2023-02-02 18:12:56 -0500

    initial setup

---
+++ warehouse-service/src/test/java/com/tudou/tudoumall/warehouseservice/WarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudou.tudoumall.warehouseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
