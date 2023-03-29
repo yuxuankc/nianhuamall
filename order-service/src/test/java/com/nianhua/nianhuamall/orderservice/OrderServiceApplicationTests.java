1b5832f186982f1f244cf0e2383b62ceb79f9a4e
kaicai <cai_kai@hotmail.com>
2023-03-29 16:29:23 -0400

    initializing project

---
+++ order-service/src/test/java/com/nianhua/nianhuamall/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nianhua.nianhuamall.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
